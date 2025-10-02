3c37babaccaa6b0fb27b206eae9362f1b804e6d7
Ali Egenix <ali@egenix.com>
2025-10-02 15:29:40 +0300

    Initial Astro site

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,167 +1,134 @@
+import About from './pages/about.astro';
 import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Shopify Development',
       links: [
         {
-          text: 'SaaS',
+          text: 'Shopify Development',
           href: getPermalink('/homes/saas'),
         },
         {
-          text: 'Startup',
+          text: 'Shopify Plus Development',
           href: getPermalink('/homes/startup'),
         },
         {
-          text: 'Mobile App',
+          text: 'Shopify Speed Optimization',
           href: getPermalink('/homes/mobile-app'),
         },
         {
-          text: 'Personal',
+          text: 'Shopify Migration',
+          href: getPermalink('/homes/personal'),
+        },
+        {
+          text: 'Shopify Consulting',
           href: getPermalink('/homes/personal'),
         },
       ],
     },
     {
-      text: 'Pages',
+      text: 'AI Development',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
+          text: 'ChatGPT integration',
           href: getPermalink('/services'),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'AI chatbot development',
+          href: getPermalink('/services'),
+        },
+       
+      ],
+    },
+    {
+      text: 'Case Studies',
+      links: [
+        {
+          text: 'Pharmacy & Supplement',
+          href: getPermalink('/services'),
         },
         {
-          text: 'About us',
+          text: 'Jewelry & Accessories',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'Print on Demand',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'Textile & Apparel',
+          href: getPermalink('/services'),
+        },
+        {
+          text: 'Home & Living',
           href: getPermalink('/about'),
         },
         {
-          text: 'Contact',
+          text: 'Checmical & Industrial',
           href: getPermalink('/contact'),
         },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
+         {
+          text: 'Leather & Luxury',
+          href: getPermalink('/contact'),
         },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+           {
+          text: 'Other Categories',
+          href: getPermalink('/contact'),
         },
+       
       ],
     },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
+     {
+      text: 'Our Shopify Themes',
+      href: getBlogPermalink(),
     },
-    {
+    
+     {
       text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
           href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
     },
-    {
-      text: 'Widgets',
-      href: '#',
+     {
+      text: 'About',
+       href: getPermalink('/about'),
     },
+    
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  actions: [{ text: 'GET A QUOTE', href:getPermalink('/getaquote'), target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Services',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Shopify Development', href: '#' },
+        { text: 'Shopify Plus Development', href: '#' },
+        { text: 'Shopify Speed Optimization', href: '#' },
+        { text: 'Shopify Migration', href: '#' },
+        { text: 'Shopify Theme Development', href: '#' },
+        { text: 'Shopify Consulting', href: '#' },
+        { text: 'AI Chatbot Development', href: '#' },
+        { text: 'Shopify & ChatGPT Integraion', href: '#' },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Our Shopify Themes',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
+        { text: 'Themes', href: '#' },
+        { text: 'Help Center', href: '#' },
+        { text: 'Documentation', href: '#' },
+        { text: 'Help Center', href: '#' },
       ],
     },
     {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
+      title: 'MIORA SOFT',
       links: [
         { text: 'About', href: '#' },
         { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Collabrations', href: '#' },
+        { text: 'Contact', href: '#' },
       ],
     },
   ],
@@ -173,10 +140,9 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    Made by Ali GUNES @ 2025
   `,
 };
