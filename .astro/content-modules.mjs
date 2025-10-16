fdb1ed6e0e7ef52ad99e611caa06399691c898ff
Ali Egenix <ali@egenix.com>
2025-10-16 15:39:37 +0300

    chore: local updates

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/data/post/migration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpost%2Fmigration.mdx&astroContentModuleFlag=true")],
 ["src/data/post/markdown-elements-demo-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpost%2Fmarkdown-elements-demo-post.mdx&astroContentModuleFlag=true")]]);
 		
